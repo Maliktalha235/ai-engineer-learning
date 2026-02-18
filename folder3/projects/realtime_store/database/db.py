258461da1e069946fb65a2953353132687dcdd37
Maliktalha235 <malicktalha095@gmail.com>
2026-02-18 16:30:18 +0500

    updating files
    Updated database connection password to 'password'.

--- folder3/projects/realtime_store/database/db.py
+++ folder3/projects/realtime_store/database/db.py
@@ -4,9 +4,10 @@ def get_connection():
     conn=mysql.connector.connect(
             host="localhost",
             user="root",
-            password="Hitec@123",
+            password="password",
             database="realtime_store"
     )
     if conn.is_connected():
         print("Connnection established...")
+
     return conn
