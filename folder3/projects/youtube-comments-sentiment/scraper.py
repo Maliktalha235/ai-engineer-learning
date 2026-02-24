54b3cffad4ca798d961ed79718ceabd7bd95af7f
Maliktalha235 <malicktalha095@gmail.com>
2026-02-24 13:29:02 +0500

    files uploaded for youtube comments sentiment analysis

--- folder3/projects/youtube-comments-sentiment/scraper.py
+++ folder3/projects/youtube-comments-sentiment/scraper.py
@@ -34,46 +34,7 @@ def fetch_comments():
 
 
    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# import requests
-# from database import create_database,create_table,get_connection,insert_comments
-# API_KEY="AIzaSyBAdAdOZ1B2rQOKPV6hO0XarqY844W6gRQ"
-# VIDEO_ID="dvUPDxfrEHM"
-# url=" https://www.googleapis.com/youtube/v3/commentThreads"
-
-
-# params={
-#     "part":"snippet",
-#     "videoId": VIDEO_ID,
-#     "maxResults":50,
-#     "key":API_KEY
-#     }
-# response=requests.get(url,params=params)
-# data=response.json()
-
-# comments=[]
-# for item in data["items"]:
-#     comment=item["snippet"]["topLevelComment"]["snippet"]["textDisplay"]
-#     comments.append(comment)
-# print(f"Fetched {len(comments)} comments")
-# print(comments[:5])
-
-# # Database creation and insertion:
-# create_database()
 # create_table()
 # print("Inserting into database:")
 # insert_comments(comments)
+
